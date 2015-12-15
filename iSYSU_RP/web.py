a35d620d79e4c8d0326548f785061f84600b374c
Matthew1994 <1037745266@qq.com>
2015-12-15 13:47:02 +0800

    rp new version

--- iSYSU_RP/web.py
+++ iSYSU_RP/web.py
@@ -8,15 +8,33 @@ from tornado.concurrent import Future
 from tornado import gen
 from tornado.options import define, options, parse_command_line
 
-define("port", default=8000, help="run on the given port", type=int)
+define("port", default=8888, help="run on the given port", type=int)
 
 
 class QueryHandler(tornado.web.RequestHandler):
 	def get(self, word):
-		self.render('RPIndex.html');
+		dic = {"a":"-1000", "b":"-520", "c":"-0.01", "d":"1",
+			   "e":"11.11", "f":"12", "g":"50", "h":"78", "i":"120",
+			   "j":"1234", "k":"110", "l":"119", "m":"80", "n":"47", 
+			   "o":"59", "p":"666", "q":"15", "r":"2000", "s":"500",
+			   "t":"2121", "u":"100000+", "v":"365", "w":"-0.00001",
+			   "x":"0", "y":"4", "z":"2333"}
+		word = word.lower()
+		self.render('result.html', char=word[0], value=dic[word[0]]);
 
 class RPIndexHandler(tornado.web.RequestHandler):
 	def get(self):
+		url = "http://isysu.sysu.edu.cn/RPTest"
+		post_data = {}
+		for key in self.request.arguments:
+			post_data[key] = self.get_arguments(key)[0]
+		if len(post_data) is not 0:
+			print "len(post_data) is not 0"
+			url = url+'?'
+			for key in post_data:
+				url = url + str(key) + "=" + str(post_data[key]) + "&"
+			url = url[0:len(url)-1]
+		print url
 		self.render('RPIndex.html');
 
 """
